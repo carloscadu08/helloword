839abce8d1e4d3777e04d3f460a039b75097a11c
carloscadu08 <60981587+carloscadu08@users.noreply.github.com>
2020-02-12 15:03:33 -0300

    Add files via upload

---
+++ tarefa1/src/tarefa1/Tarefa1.java
@@ -0,0 +1,15 @@
+
+package tarefa1;
+
+
+public class Tarefa1 {
+
+    public static void main(String[] args) {
+        System.out.println("Desenvolva um programa para apresentar ao professor");
+        System.out.println("desenrolar do projeto");
+        int a;
+        int b;
+        
+    }
+    
+}
